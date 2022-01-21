8779891dc905832a039a1536f2a4083f810557b8
modabada <miw1102@naver.com>
2022-01-21 22:07:13 +0900

    검색기능 추가

--- binance.js
+++ binance.js
@@ -3,14 +3,14 @@ importPackage(org.jsoup);
 const allsee = "\u200b".repeat(500);
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-    if (msg == "코인 시세") {
+    if (msg.startsWith("코인 시세")) {
         replier.reply("데이터를 가져오는 중입니다...");
-        replier.reply(binance());
+        replier.reply(binance(msg.slice(6).toUpperCase()));
     }
 }
 
 
-function binance() {
+function binance(code) {
     const line = "=".repeat(20);
     const data = JSON.parse(
         Jsoup.connect("https://api.binance.com/api/v3/ticker/24hr")
@@ -18,8 +18,11 @@ function binance() {
             .get().text()
     );
     let res = "Binance 시세조회\n" + allsee + "\n" + line + "\n\n";
-    data.forEach((e) => {
+    for(e of data) {
+        if(code == e.symbol) {
+            return "Binance 시세조회 " + code + "\n" + allsee + "\n" + line + "\n\n" + "종목: " + e.symbol + "\n\n전일 종가: " + +e.prevClosePrice + "\n금일 고가: " + +e.highPrice + "\n금일 저가: " + +e.lowPrice + "\n\n구매가: " + +e.askPrice + "\n판매가: " + +e.bidPrice + "\n등락폭: " + +e.priceChangePercent + "%\n\n" + line + "\n\n";
+        }
         res += "종목: " + e.symbol + "\n\n전일 종가: " + +e.prevClosePrice + "\n금일 고가: " + +e.highPrice + "\n금일 저가: " + +e.lowPrice + "\n\n구매가: " + +e.askPrice + "\n판매가: " + +e.bidPrice + "\n등락폭: " + +e.priceChangePercent + "%\n\n" + line + "\n\n";
-    });
+    }
     return res;
 }
