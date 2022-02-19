0f7ee7b95fd32753334a8d54fc8a43b687c366c3
modabada <69136135+modabada@users.noreply.github.com>
2022-02-19 21:47:45 +0900

    자동 업데이트로 변경
    채팅이 올 때마다 회차기록 가져와서 새 게임이 진행되었다면 자동 알람

--- ntry.js
+++ ntry.js
@@ -1,39 +1,44 @@
-const scriptName = "Jacqueline2.0";
+const scriptName = "ntry";
 
 importPackage(org.jsoup);
+ntry_obj = {
+    last_round: 0,
+    last_time: 0
+};
 
 function response(room, msg, sender, isGroupChat, replier, imageDB, packageName) {
-    switch (msg) {
-        case "사다리":
-            replier.reply(ntry("ladder"));
-            break;
-        case "파워볼":
-            replier.reply(ntry("ball"));
-            break;
-    }
+    ntry(replier);
 }
 
 
-function ntry(item) {
-    let data;
-    switch (item) {
-        case "ball":
-            data = JSON.parse(Jsoup.connect("https://ntry.com/data/json/games/powerball/result.json").ignoreContentType(true).get().text());
-            return data.date_round + "회차 파워볼\n" + [
-                data.pow_ball_oe ? data.pow_ball_oe : "홀",
-                data.pow_ball_unover === "언더" ? "언" : "오",
-                data.def_ball_size ? data.def_ball_size : "소",
-                data.def_ball_oe ? data.def_ball_oe : "홀",
-                data.def_ball_unover === "언더" ? "언" : "오"
-            ].join(" ");
-        case "ladder":
-            data = JSON.parse(Jsoup.connect("https://ntry.com/data/json/games/power_ladder/result.json").ignoreContentType(true).get().text());
-            return data.r + "회차 사다리\n" + [
-                (data.s ? data.s : 'LEFT') == "LEFT" ? "좌" : "우",
-                data.l ? data.l : '3',
-                (data.o ? data.o : 'ODD') == "ODD" ? "홀" : "짝"
-            ].join(" ");
-        default:
-            throw Error();
+function ntry(replier) {
+    let time = Date.now();
+    if (time - ntry_obj.last_time < 10 * 1000) { // 딜레이 시간(초) * 1000
+        return;
     }
+
+    const ball = JSON.parse(Jsoup.connect("https://ntry.com/data/json/games/powerball/result.json").ignoreContentType(true).get().text());
+
+    if (ball.date_round == ntry_obj.last_round) {
+        return;
+    }
+
+    const ladder = JSON.parse(Jsoup.connect("https://ntry.com/data/json/games/power_ladder/result.json").ignoreContentType(true).get().text());
+    ntry_obj.last_time = time;
+    ntry_obj.last_round = ball.date_round;
+
+    replier.reply(
+        ball.date_round + "회차 파워볼\n" + [
+            ball.pow_ball_oe ? ball.pow_ball_oe : "홀",
+            ball.pow_ball_unover === "언더" ? "언" : "오",
+            ball.def_ball_size ? ball.def_ball_size : "소",
+            ball.def_ball_oe ? ball.def_ball_oe : "홀",
+            ball.def_ball_unover === "언더" ? "언" : "오"
+        ].join(" ") + "\n" + "=".repeat(20) + "\n" +
+        ladder.r + "회차 사다리\n" + [
+            (ladder.s ? ladder.s : 'LEFT') == "LEFT" ? "좌" : "우",
+            ladder.l ? ladder.l : '3',
+            (ladder.o ? ladder.o : 'ODD') == "ODD" ? "홀" : "짝"
+        ].join(" ")
+    );
 }
